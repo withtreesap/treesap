523f9af517954cc11447536da351ecda9db594fe
Jaydan Urwin <jaydan@treefarm.studio>
2024-10-04 12:20:39 -0400

    sidebar cleanup

--- examples/astro-cms/src/components/cms/sidebar-content.tsx
+++ examples/astro-cms/src/components/cms/sidebar-content.tsx
@@ -17,29 +17,29 @@ export function SidebarContent({ navData }: SidebarContentProps) {
 
   return (
     <nav className="grid items-start px-2 text-sm font-medium lg:px-4 gap-2">
-      <div className="flex flex-col gap-2">
-        <a
-          href={`/admin/settings`}
-          className={`flex items-center gap-3 rounded-lg px-3 py-2 t transition-all  ${location.pathname === `/admin/settings` ? "bg-primary/10 text-primary" : ``}`}
-        >
-          <Settings className="h-4 w-4" />
-          Settings
-        </a>
-        <a
-          href={`/admin/users`}
-          className={`flex items-center gap-3 rounded-lg px-3 py-2 t transition-all  ${location.pathname === `/admin/users` ? "bg-primary/10 text-primary" : ``}`}
-        >
-          <Users className="h-4 w-4" />
-          Users
-        </a>
-        <a
-          href={`/admin/media`}
-          className={`flex items-center gap-3 rounded-lg px-3 py-2 t transition-all  ${location.pathname === `/admin/media` ? "bg-primary/10 text-primary" : ``}`}
-        >
-          <Image className="h-4 w-4" />
-          Media
-        </a>
-      </div>
+
+      <a
+        href={`/admin/settings`}
+        className={`flex items-center gap-3 rounded-lg px-3 py-2 t transition-all  ${location.pathname === `/admin/settings` ? "bg-primary/10 text-primary" : ``}`}
+      >
+        <Settings className="h-4 w-4" />
+        Settings
+      </a>
+      <a
+        href={`/admin/users`}
+        className={`flex items-center gap-3 rounded-lg px-3 py-2 t transition-all  ${location.pathname === `/admin/users` ? "bg-primary/10 text-primary" : ``}`}
+      >
+        <Users className="h-4 w-4" />
+        Users
+      </a>
+      <a
+        href={`/admin/media`}
+        className={`flex items-center gap-3 rounded-lg px-3 py-2 t transition-all  ${location.pathname === `/admin/media` ? "bg-primary/10 text-primary" : ``}`}
+      >
+        <Image className="h-4 w-4" />
+        Media
+      </a>
+
       <div className="flex items-center justify-between">
         <h3 className="text-muted-foreground">Globals</h3>
         <a

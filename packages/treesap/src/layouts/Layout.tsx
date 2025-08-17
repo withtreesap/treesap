30d948742205b4f967fe4da11cc0d0b9f5794379
Jaydan Urwin <jaydan@treefarm.studio>
2025-08-16 22:52:33 -0600

    remove sapling dependency

--- packages/treesap/src/layouts/Layout.tsx
+++ packages/treesap/src/layouts/Layout.tsx
@@ -1,34 +1,41 @@
-import {
-  Layout as SaplingLayout,
-  type LayoutProps,
-} from "@sapling/sapling";
-import { html } from "hono/html";
-import { BaseHead } from "../components/BaseHead.js";
-
-export type BaseLayoutProps = LayoutProps & {
+interface BaseLayoutProps {
   title?: string;
   description?: string;
-  additionalHead?: string;
-};
+  head?: any;
+  children: any;
+}
 
-export default async function Layout(props: BaseLayoutProps) {
-  const defaultHeadContent = BaseHead({
-    title: props.title,
-    description: props.description,
-  });
-
-  const combinedHead = html`
-    ${defaultHeadContent}
-    ${props.additionalHead || ''}
-  `;
-
+export default function BaseLayout(props: BaseLayoutProps) {
   return (
-    <SaplingLayout
-      enableIslands
-      head={combinedHead}
-      bodyClass={props.bodyClass || `font-sans @dark:bg-black @dark:text-white`}
-    >
-      {props.children} 
-    </SaplingLayout>
+    <>
+      <div dangerouslySetInnerHTML={{ __html: '<!DOCTYPE html>' }} />
+      <html lang="en">
+      <head>
+        <title>{props.title || "Treesap"}</title>
+        <meta name="description" content={props.description || "A modern web application"} />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/styles/main.css" />
+        {/* Sapling Islands */}
+        <script type="module" src="https://sapling-is.land"></script>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              sapling-island{display:contents}
+            `,
+          }}
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/iconify-icon@2.1.0/dist/iconify-icon.min.js"
+          defer
+        >
+        </script>
+        {props.head}
+      </head>
+      <body class="bg-white text-black">
+        {props.children}
+      </body>
+      </html>
+    </>
   );
 }
